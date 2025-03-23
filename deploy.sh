f990e8e0136e2b9fe9ddf97a6920c458123a35d5
EkaterinaBatullina <batullina2005@gmail.com>
2025-03-23 15:07:21 +0300

    infrastructure

--- deploy.sh
+++ deploy.sh
@@ -23,15 +23,15 @@ ssh "$SSH" "sudo rm -r infrastructure2/ || echo 0"
 
 ssh "$SSH" "GIT_SSH_COMMAND='ssh -i /home/eka_rina16/.ssh/id_rsa_vm' git clone git@gitlab.com:EkaterinaBatullina/infrastructure2.git"
 
-ssh "$SSH" "cd ~/Agona-05/ && git checkout $BRANCH"
+ssh "$SSH" "cd ~infrastructure2/Agona-05/ && git checkout $BRANCH"
 
 ssh "$SSH" "sudo docker login -u gitlab-ci-token -p glpat-8QYBRfe7gSiqMesP9fee $CI_REGISTRY"
 
-ssh "$SSH" "sudo docker pull registry.gitlab.com/batullina-agona-2024/agona-05:$TAG"
+ssh "$SSH" "sudo docker pull registry.gitlab.com/EkaterinaBatullina/infrastructure2/agona-05:$TAG"
 
-ssh "$SSH" "sudo docker-compose -p $BRANCH -f ~/batullina-agona-2024/agona-05/docker/docker-compose.$TAG.yml down"
+ssh "$SSH" "sudo docker-compose -p $BRANCH -f ~/infrastructure2/docker/docker-compose.$TAG.yml down"
 
-ssh "$SSH" "sudo docker-compose -p $BRANCH -f ~/batullina-agona-2024/agona-05/docker/docker-compose.$TAG.yml up --no-build -d"
+ssh "$SSH" "sudo docker-compose -p $BRANCH -f ~/infrastructure2/docker/docker-compose.$TAG.yml up --no-build -d"
 
 ssh "$SSH" "sudo rm -r infrastructure2/"
 
