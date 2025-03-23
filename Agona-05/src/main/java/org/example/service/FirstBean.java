62b2e44419b58860e559e602d6e973016fcb18d7
EkaterinaBatullina <batullina2005@gmail.com>
2025-03-23 11:28:30 +0300

    infrastructure

---
+++ Agona-05/src/main/java/org/example/service/FirstBean.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+public class FirstBean {
+
+    public FirstBean() {
+        System.out.println("FirstBean initialized");
+    }
+
+}
---
+++ Agona-05/src/main/java/org/example/service/SecondBean.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+public class SecondBean {
+
+    public SecondBean() {
+        System.out.println("SecondBean initialized");
+    }
+
+}
