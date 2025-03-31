ee332032a6a13e4e62e7270507f677a75c499911
EkaterinaBatullina <batullina2005@gmail.com>
2025-03-31 11:52:31 +0300

    infrastructure

---
+++ user-service/user-service-api/src/main/java/org/example/dto/request/UserRequest.java
@@ -0,0 +1,4 @@
+package org.example.dto.request;
+
+public record UserRequest(String username, String email, String password, String role) {}
+
---
+++ user-service/user-service-api/src/main/java/org/example/dto/response/UserResponse.java
@@ -0,0 +1,3 @@
+package org.example.dto.response;
+
+public record UserResponse(String username, String email, String password, String role) {}
